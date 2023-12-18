7aaf4116630c0f2bc7a4c74481dacf1e27300109
manuel Gonzalez Fontaiña <alvaroga2003@gmail.com>
2023-12-18 13:21:57 +0100

    Mejorada legibilidad del código

--- src/AlumnoCRUD.java
+++ src/AlumnoCRUD.java
@@ -80,4 +80,11 @@ listaAlumnos.set(i, alumno);
 }
 escribirAlumnos(listaAlumnos);
 }
+public void bajaAlumno(String dni) {
+ArrayList<Alumno> listaAlumno = leerAlumnos();
+for (int i = 0; i < listaAlumno.size(); i++)
+if (listaAlumno.get(i).getDni().equalsIgnoreCase(dni))
+listaAlumno.remove(i);
+escribirAlumnos(listaAlumno);
+}
 }
