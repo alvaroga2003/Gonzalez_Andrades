748ff4127dc7d43017151f3717f73c7266eef781
Zentri G <pablogonzalezsilva6@gmail.com>
2023-12-18 12:51:33 +0100

    Añadido método bajaAlumno

--- src/AlumnoCRUD.java
+++ src/AlumnoCRUD.java
@@ -61,4 +61,13 @@ listaAlumno.add(alumno);
 escribirAlumnos(listaAlumno);
 }
 
+public void bajaAlumno(String dni) {
+ArrayList<Alumno> listaAlumno = leerAlumnos();
+for (int i = 0; i < listaAlumno.size(); i++) {
+if (listaAlumno.get(i).getDni().equalsIgnoreCase(dni)) {
+listaAlumno.remove(i);
+}
+}
+escribirAlumnos(listaAlumno);
+}
 }
